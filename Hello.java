3f94edcd0a543355834e871f2f0dc5799bfe8ca4
rachidjnaou <rxflash@gmail.com>
2013-02-26 16:09:01 +0100

    Revert "code source coms"
    This reverts commit ebfe7e1b8aca72345aa316c00e735f5f7ca98e70.

--- Hello.java
+++ Hello.java
@@ -1,10 +1,7 @@
-// Clas3 Hello mostrar un missatge, fet per RAR inc.
 class Hello
 {
-  // main methode
   public static void main ( String[] args )        
   {
-    // display "Hello wold" message.
     System.out.println("Hello world");
   }
 }
