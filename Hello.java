ebfe7e1b8aca72345aa316c00e735f5f7ca98e70
rachidjnaou <rxflash@gmail.com>
2013-02-26 16:08:49 +0100

    code source coms
    modified by Rachid

--- Hello.java
+++ Hello.java
@@ -1,7 +1,10 @@
+// Clas3 Hello mostrar un missatge, fet per RAR inc.
 class Hello
 {
+  // main methode
   public static void main ( String[] args )        
   {
+    // display "Hello wold" message.
     System.out.println("Hello world");
   }
 }
